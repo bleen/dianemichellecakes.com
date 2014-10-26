b0f0d9b4513c56938e1d4ee62f037c06cabc5ea6
bleen <git.119x@bleen.net>
2014-10-25 21:12:31 -0400

    Adding drush aliases

---
+++ docroot/sites/all/drush/dmc.aliases.drushrc.php
@@ -0,0 +1,19 @@
+<?php
+
+/* ==========================  L O C A L  ========================= */
+$aliases['local'] = array(
+  'env' => 'local',
+  '%dump-dir' => '/tmp/php',
+);
+
+
+/* ===========================  P R O D  ========================== */
+$aliases['prod'] = array(
+  'env' => 'prod',
+  'root' => '/home/fehder/sites/dianemichellecakes.com/docroot',
+  'path-aliases' => array(
+    '%dump-dir' => '/tmp',
+  ),
+  'remote-host' => 'dianemichellecakes.com',
+  'remote-user' => 'fehder',
+);
